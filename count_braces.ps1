8baea77b2527c8c210c4307b8e4d0b466fc50ff1
LilCodru <romascodrut625@gmail.com>
2025-09-30 23:24:09 +0300

    -

--- count_braces.ps1
+++
@@ -1,6 +0,0 @@
-$path = "D:\Proiecte-C++\High-precision-AOCS\src\cpp\aocs_realtime_simulator_integrated.cpp"
-$text = Get-Content -Raw $path
-$chars = $text.ToCharArray()
-$open = ($chars | Where-Object { $_ -eq '{' }).Count
-$close = ($chars | Where-Object { $_ -eq '}' }).Count
-Write-Output "open={$open} close={$close}"
--- dist/Launch_AOCS_Simulator.bat
+++
@@ -1,5 +0,0 @@
-@echo off 
-echo Starting AOCS Simulator... 
-echo Press ESC to exit fullscreen mode. 
-echo. 
-start "" "AOCS_Simulator.exe" 
